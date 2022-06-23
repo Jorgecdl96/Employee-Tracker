71a4c3730434e63be6a1b141b1875da8b866094f
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-06-22 22:11:44 -0600

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -15,13 +15,15 @@ CREATE TABLE role (
     department_id INT NOT NULL,
     FOREIGN KEY(department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL FOREIGN KEY REFERENCES role(id),
-    manager_id INT NOT NULL REFERENCES employee(id);
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL FOREIGN KEY
+    REFERENCES employee(id)
+    ON UPDATE CASCADE ON DELETE CASCADE;
 );
